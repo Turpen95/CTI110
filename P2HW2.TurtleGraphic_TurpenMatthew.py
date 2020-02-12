a1fde933cfb4bc8e6c3b8f04502a1f562ce6d8b3
Turpen95 <60890591+Turpen95@users.noreply.github.com>
2020-02-12 11:40:32 -0500

    Add files via upload

---
+++ P2HW2.TurtleGraphic_TurpenMatthew.py
@@ -0,0 +1,30 @@
+    #TurtleGraphics
+    #February 10, 2020
+    #CTI-110 P2HW2 - Turtle Graphic
+    #Matthew Turpen
+
+import turtle
+def main():
+    turtle.showturtle()
+    turtle.bgcolor('grey')
+    turtle.pensize(2)
+    turtle.fillcolor('white')
+    turtle.begin_fill()
+    turtle.right(45)
+    turtle.forward(200)
+    turtle.left(90)
+    turtle.forward(200)
+    turtle.left(90)
+    turtle.forward(200)
+    turtle.left(90)
+    turtle.forward(400)
+    turtle.right(90)
+    turtle.forward(200)
+    turtle.right(90)
+    turtle.forward(200)
+    turtle.right(90)
+    turtle.forward(200)
+    turtle.end_fill()
+
+main()
+
