9bbd2720200f51277f017b9df50aaefa75bfba51
Turpen95 <60890591+Turpen95@users.noreply.github.com>
2020-03-02 12:24:07 -0500

    Add files via upload
    A program that calculates the total amount of bugs a user would collect over a 5 day period.

---
+++ P4T2_BugCollector_MatthewTurpen.py
@@ -0,0 +1,25 @@
+#Bug Collector
+#March 2, 2020
+#CTI-110 P4T2 - Bug Collector
+#Matthew Turpen
+
+def main():
+#Initrialize the accumulator.
+    total = 0
+
+#Get the bugs collected for each day.
+    for day in range(1, 6):
+        #Prompt the user.
+        print('Enter the bugs collected on day', day)
+
+        #Input the number of bugs.
+        bugs = int(input())
+
+        #Add bugs to total.
+        total += bugs
+        
+#Display the total bugs.
+    print('You collected a total of', total, 'bugs.')
+main()
+                        
+                        
